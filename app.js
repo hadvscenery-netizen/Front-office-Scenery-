aff0188d3a1bfa8e20609a72f6b01abf2a2a8edb
hadvscenery-netizen <h.adv.scenery@gmail.com>
2026-08-15 13:41:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* Loader for the split application bundle. */
 (function () {
   const parts = ["app-part-01.js","app-part-02.js","app-part-03.js","app-part-04.js","app-part-05.js","app-part-06.js","app-part-07.js","app-part-08.js","app-part-09.js"];
-  parts.forEach((src) => document.write('<script src="' + src + '"><\/script>'));
+  const build='20260815-a4-print-2';
+  parts.forEach((src) => document.write('<script src="' + src + '?v=' + build + '"><\/script>'));
 }());
